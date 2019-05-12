922ee9c3480d51870b787db1b4f3dc7544c5eda2
Gus Mendez <gus.mendez.99@gmail.com>
2019-05-11 21:18:54 -0600

    Megaman 4 NES - minor bugs and errors

---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int life;
+	public GameObject fishPrefab;
+	public Transform fishesGenerator;
+	public float interval;
+
+	IEnumerator Start() {
+		Instantiate (fishPrefab, fishesGenerator.transform.position, fishesGenerator.transform.rotation);
+		yield return new WaitForSeconds (interval);
+
+		StartCoroutine (Start ());
+	}
+
+	void OnCollisionEnter2D (Collision2D c) {
+		if (c.gameObject.tag == "Fire") {
+			life--;
+			if (life <= 0) {
+				//Player has won
+				Destroy (gameObject);
+				GameObject.Find("EndText").GetComponent<Text>().text = "You won!!!";
+				GameController.Instance.userHasWon = true;
+			}
+		}
+	}
+}
---
+++ Assets/Script/GameScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject player, currPlayer;
+    public Vector3 initialPosition;
+    private static GameController _instance;
+    public bool userHasWon = false;
+
+    public static GameController Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player != null)
+        {
+            initialPosition = player.transform.position;
+            currPlayer = Instantiate(player, new Vector2(0f,0f), Quaternion.identity);
+
+        }
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player != null)
+        {
+            initialPosition = player.transform.position;
+            currPlayer = Instantiate(player, new Vector2(9.5f, -1f, 23.1f), Quaternion.identity);
+
+        }
+    }
+
+    public void RestartPlayer()
+    {
+        if (userHasWon)
+        {
+            Destroy(currPlayer);
+            userHasWon = false;
+            GameObject.Find("EndText").GetComponent<Text>().text = "";
+            currPlayer = Instantiate(player, new Vector2(initialPosition.x, -initialPosition.y), Quaternion.identity);
+        }
+        else {
+            if (player && !currPlayer)
+            {
+                GameObject.Find("EndText").GetComponent<Text>().text = "";
+                currPlayer = Instantiate(player, new Vector2(initialPosition.x, -initialPosition.y), Quaternion.identity);
+            }
+        }
+
+        //Finally
+        Time.timeScale = 1.0f;
+}
+}
